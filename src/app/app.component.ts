4263f0fab8eb3e8a8260bcf6c913623b6f091d8b
ShaikhTanishq <ShaikhTanishq@github.com>
2023-12-14 18:34:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,202 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { animate, state, style, transition, trigger } from "@angular/animations";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+//import { UploadService } from './upload.service';
+
+// interface IUser {
+//   firstname: string;
+//   lastname: string;
+//   email: string;
+//   password: string;
+  
+// }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger("expandCollapseDetails", [
+      state(
+        "void",
+        style({
+          height: "0px",
+          overflow: "hidden"
+        })
+      ),
+      //element being added into DOM.
+      transition(":enter", [
+        animate(
+          "500ms ease-in-out",
+          style({
+            height: "*",
+            overflow: "hidden"
+          })
+        )
+      ]),
+      //element being removed from DOM.
+      transition(":leave", [
+        animate(
+          "500ms ease-in-out",
+          style({
+            height: "0px",
+            overflow: "hidden"
+          })
+        )
+      ])
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'reactiveForms';
-}
+export class AppComponent implements OnInit {
+  @ViewChild('fileInput') fileInput: ElementRef;
+showMessage: string;
+gender: any = 'FEMALE'
+title = 'reactiveForms';
+  reactiveForm: FormGroup;
+// user: IUser;
+  constructor() {
+
+  }
+
+  ngOnInit(){
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(10),
+      ]),
+      mobile: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")
+      ]),
+      // gender: new FormControl(null, [
+      //   Validators.required
+      // ]),
+      lastname: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.minLength(1)
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        // Validators.maxLength(250),
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+      ]),
+      qualification: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25)
+      ]),
+      category: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25)
+      ]),
+      currentcompany: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+      ]),
+      currentloc: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+      ]),
+      currentsalary: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25),
+      ]),
+      skills: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25)
+      ]),
+      currentdesignation: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25)
+      ]),
+      yearsofexp: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(6)
+      ]),
+      noticeperiod: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25)
+      ]),
+      expectedsalary: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(6),
+      ]),
+      prjs: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25)
+      ]),
+      project: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(25)
+      ]),
+      gender: new FormControl(null, Validators.required)
+    });
+  }
+
+  
+  onSubmit2() {
+    console.log(this.reactiveForm);
+    // alert(this.invalidMessage);
+  }
+  
+  add(){ 
+    let row = document.createElement('div');   
+    row.className = 'row'; 
+    row.innerHTML = `<input type="text" placeholder="Project">`; 
+    document.querySelector('.showInputField').appendChild(row); 
+  } 
+  
+  onFileChange(event: any) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    this.reactiveForm.patchValue({
+      resumeFile: file,
+    });
+    this.reactiveForm.get('resumeFile').updateValueAndValidity();
+  }
+
+  uploadFullResume() {
+    this.fileInput.nativeElement.click();
+  }
+
+  uploadBlankResume() {
+    this.fileInput.nativeElement.click();
+  }
+  
+  uploadImage() {
+    this.fileInput.nativeElement.click();
+  }
+            onSubmit() {
+              console.log(this.reactiveForm);
+            
+              if (this.reactiveForm.valid) {
+                this.showMessage = 'Form is Valid';
+              } else {
+                this.showMessage = 'Form is Invalid';
+              }
+            }
+            
+            here(){
+              console.log('here');
+              
+            }
+          } 
+
+        
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import{ MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { AddInputComponent } from './add-input/add-input.component'; 
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    FormsModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
